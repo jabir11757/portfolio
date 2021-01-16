61ad15bdfb9a046e01c0eb4f3b5e68f9cdf19a1f
jabir11757 <smjalsaba11757@gmail.com>
2021-01-15 22:40:01 -0800

    1/15/21

--- src/components/main.js
+++ src/components/main.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Port from './typed';
-import HookCounter from './state'
+
 
 const Main = () => {
     return ( 
         <div>
             <Port/>
-            <HookCounter/>
+        
         </div>
      );
 }
